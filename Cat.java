626723a5037597c386842c35cf7084aea36d9c28
Nick-Morbid <78581940+Nick-Morbid@users.noreply.github.com>
2021-02-05 13:45:03 +0800

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal{
+    public Cat(int number,int age,String kind,double price,String state1,int kindnumber,String state2){
+        super(number,age,kind,price,state1,kindnumber,state2);
+    }
+    @Override
+    public String toString(){
+        if(state1.equals("买入")||state1.equals("在套餐中"))
+            return "有"+state1+"的"+number+"条年龄为"+age+"岁、买入价格为"+price+"元/条的"+kind+" 是否打疫苗："+state2;
+        return "有"+state1+"的"+number+"条年龄为"+age+"岁、寄养价格为"+price+"元/条的"+kind+" 是否打疫苗："+state2;
+    }
+}
